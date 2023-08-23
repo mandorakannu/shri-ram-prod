1bc7c04aa31f7db0d3fde94c39a067cbc531ab88
mandorakannu <mandorakannu@gmail.com>
2023-08-23 23:17:54 +0530

    Add  auth routes in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import express, { Application, Request, Response } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import { resolve, join } from "path";
+import { resolve } from "path";
+import * as dotenv from "dotenv";
+dotenv.config();
+import {
+  authStudentRoute,
+  authTeacherRoute,
+  authAdminRoute,
+} from "@routes/login";
 
 const app: Application = express();
 const PORT = process.env.PORT || 8000;
@@ -10,7 +17,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static("public"));
-join(__dirname, "public");
+app.use(authStudentRoute);
+app.use(authTeacherRoute);
+app.use(authAdminRoute);
 
 app.get("*", (request: Request, res: Response) => {
   res.sendFile(resolve(__dirname, "index.html"));
